ca6845b49ac8d1610c79789d6ab812dcf5cf2c5b
Mike Stuck <mikestuck@msn.com>
2016-10-25 13:00:46 -0700

    Initial Commit

---
+++ EaglesSchedule - ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EaglesSchedule - ObjC
+//
+//  Created by Mike Stuck on 10/25/16.
+//  Copyright © 2016 Mike Stuck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EaglesSchedule - ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EaglesSchedule - ObjC
+//
+//  Created by Mike Stuck on 10/25/16.
+//  Copyright © 2016 Mike Stuck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
